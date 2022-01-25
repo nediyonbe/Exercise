dd3da2a07edbda12475a9625f9d6df4bf288278e
nediyonbe <gurkanali@gmail.com>
2022-01-25 17:56:13 -0500

    Designer Door Mat

---
+++ Exercise 18.py
@@ -0,0 +1,67 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input().split()[0])
+# print(N)
+M = N * 3
+r = '.|.'
+
+for i in range(int((N - 1)/2)):
+    print((r*(2*i+1)).center(M,'-'))
+
+print('WELCOME'.center(M, '-'))
+
+for i in reversed(range(int((N - 1)/2))):
+    print((r*(2*i+1)).center(M,'-'))
+
+# Designer Door Mat
+# Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+# Mat size must be X. ( is an odd natural number, and  is  times .)
+# The design should have 'WELCOME' written in the center.
+# The design pattern should only use |, . and - characters.
+# Sample Designs
+
+#     Size: 7 x 21 
+#     ---------.|.---------
+#     ------.|..|..|.------
+#     ---.|..|..|..|..|.---
+#     -------WELCOME-------
+#     ---.|..|..|..|..|.---
+#     ------.|..|..|.------
+#     ---------.|.---------
+    
+#     Size: 11 x 33
+#     ---------------.|.---------------
+#     ------------.|..|..|.------------
+#     ---------.|..|..|..|..|.---------
+#     ------.|..|..|..|..|..|..|.------
+#     ---.|..|..|..|..|..|..|..|..|.---
+#     -------------WELCOME-------------
+#     ---.|..|..|..|..|..|..|..|..|.---
+#     ------.|..|..|..|..|..|..|.------
+#     ---------.|..|..|..|..|.---------
+#     ------------.|..|..|.------------
+#     ---------------.|.---------------
+# Input Format
+
+# A single line containing the space separated values of  and .
+
+# Constraints
+
+# Output Format
+
+# Output the design pattern.
+
+# Sample Input
+
+# 9 27
+# Sample Output
+
+# ------------.|.------------
+# ---------.|..|..|.---------
+# ------.|..|..|..|..|.------
+# ---.|..|..|..|..|..|..|.---
+# ----------WELCOME----------
+# ---.|..|..|..|..|..|..|.---
+# ------.|..|..|..|..|.------
+# ---------.|..|..|.---------
+# ------------.|.------------
