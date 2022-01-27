98344ab6555253ee85b1aea2fe625f69350aa800
nediyonbe <gurkanali@gmail.com>
2022-01-26 22:52:35 -0500

    Alphabet Rangoli

--- Exercise 20.py
+++ Exercise 20.py
@@ -3,14 +3,100 @@ def print_rangoli(size):
     # your code goes here
     width = 2 * 2 * (size-1) + 1
     
-    for i in range(size): #97, 123
-        #print(functools.reduce(lambda x, y:  str(chr(x)) + str(chr(y)), range(97, 97 + size + 1)))
-        liste = list(range(97 + i, 97 + size + 1))
+    # print the upper half + the middle line
+    for i in reversed(range(size)): #97, 123
+        liste = list(range(97 + i, 97 + size ))
         liste_text = [chr(x) for x in liste]
-        print(i)
-        print(functools.reduce(lambda x, y:  x + '-' + y, liste_text))
+        line_to_print = liste_text[1:][::-1] + liste_text
+        print(functools.reduce(lambda x, y:  x + '-' + y, line_to_print).center(width, '-'))
 
+    # print the lower half
+    for i in range(size-1): #97, 123
+        liste = list(range(97 + i + 1, 97 + size))
+        liste_text = [chr(x) for x in liste]
+        line_to_print = liste_text[1:][::-1] + liste_text
+        print(functools.reduce(lambda x, y:  x + '-' + y, line_to_print).center(width, '-'))
 
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
+
+# Alphabet Rangoli
+# You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+# Different sizes of alphabet rangoli are shown below:
+
+# #size 3
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
+
+# #size 5
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+# #size 10
+
+# ------------------j------------------
+# ----------------j-i-j----------------
+# --------------j-i-h-i-j--------------
+# ------------j-i-h-g-h-i-j------------
+# ----------j-i-h-g-f-g-h-i-j----------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ----------j-i-h-g-f-g-h-i-j----------
+# ------------j-i-h-g-h-i-j------------
+# --------------j-i-h-i-j--------------
+# ----------------j-i-j----------------
+# ------------------j------------------
+# The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+# Function Description
+
+# Complete the rangoli function in the editor below.
+
+# rangoli has the following parameters:
+
+# int size: the size of the rangoli
+# Returns
+
+# string: a single string made up of each of the lines of the rangoli separated by a newline character (\n)
+# Input Format
+
+# Only one line of input containing , the size of the rangoli.
+
+# Constraints
+
+
+# Sample Input
+
+# 5
+# Sample Output
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
